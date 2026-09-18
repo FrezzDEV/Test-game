8124b5d05c4d0188fedd525dfd34c874781429ec
FrezzDEV <102240197+FrezzDEV@users.noreply.github.com>
2026-09-18 21:56:03 +0300

    Start all discovered Telegram sessions

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 import asyncio
 
 from app.admin.bot import run_admin_bot
-from app.telegram.client import run_telegram_client
+from app.telegram.client import run_telegram_clients
 
 
 async def main() -> None:
     await asyncio.gather(
-        run_telegram_client(),
+        run_telegram_clients(),
         run_admin_bot(),
     )
 
